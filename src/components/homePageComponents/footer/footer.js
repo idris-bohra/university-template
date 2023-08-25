f2d396e7aea70afe01c322da9496fd55b4380cd2
idris bohra <idrisbohra786@gmail.com>
2023-08-26 03:15:10 +0530

    common header has been made and working successfully

--- src/components/homePageComponents/footer/footer.js
+++ src/components/homePageComponents/footer/footer.js
@@ -2,8 +2,8 @@
 import React from 'react';
 
 // ICON-IMPORTS.
-import { FaFacebookF } from "react-icons/fa";
-import { AiOutlineInstagram } from "react-icons/ai";
+import { CgFacebook } from "react-icons/cg";
+import { FaInstagram } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { BsSuitHeart } from "react-icons/bs";
@@ -15,11 +15,11 @@ export default function Footer() {
   return (
     <div className='footer-main-container'>
       <h3 className='footer-main-title'>About Us</h3>
-      <p>Welcome to University, where academic excellence meets transformative innovation.<br /> Join us in our journey of fostering knowledge, creativity, and leadership.</p>
+      <p>Welcome to University, where academic excellence meets transformative innovation. Join us in our journey of <br /> fostering knowledge, creativity, and leadership.</p>
       <section className='footer-icon-main-container'>
-        <FaFacebookF className='footer-icon footer-icon-facebook' />
+        <CgFacebook className='footer-icon footer-icon-facebook' />
         <FaLinkedinIn className='footer-icon footer-icon-linkedin' />
-        <AiOutlineInstagram className='footer-icon footer-icon-instagram' />
+        <FaInstagram className='footer-icon footer-icon-instagram' />
         <FaTwitter className='footer-icon footer-icon-twitter' />
       </section>
       <p className='footer-end-title'>Made with <BsSuitHeart className='footer-icon-heart' /> by Idris Bohra.</p>
